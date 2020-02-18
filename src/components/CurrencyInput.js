4cebff62f5d76142985c7c38ff066d9d21e482d2
Timur Celik <timur.celik@gmx.de>
2020-02-18 19:00:56 +0100

    added currency data and put it into autocomplete control

--- src/components/CurrencyInput.js
+++ src/components/CurrencyInput.js
@@ -2,34 +2,37 @@ import React from 'react';
 import {Container, Row, Col} from 'react-bootstrap';
 import {TextField} from '@material-ui/core';
 import Autocomplete from '@material-ui/lab/Autocomplete'
+import currencies from '../data/currencies';
 
 export default function CurrencyInput(props) {
 	return (
-			<Container>
-				<Row className="justify-content-md-center">
-					<Col sm={2}>
-					 	<TextField
-							type='number'
-							variant='outlined'
-							defaultValue={'1.00'}
-							inputProps={{readOnly: props.readOnly}}
-                			fullWidth />
-					</Col>
-					<Col sm={5}>
-					 	<Autocomplete
-							options={['EUR', 'USD']}
-							renderInput={params => (
-							  <TextField
-								{...params}
-								label="Currency"
-								variant="outlined"
-								fullWidth
-								inputProps={{ ...params.inputProps }}
-							  />
-							)}
+		<Container>
+			<Row className="justify-content-md-center">
+				<Col sm={2}>
+				 	<TextField
+						type='number'
+						variant='outlined'
+						defaultValue={'1.00'}
+						inputProps={{readOnly: props.readOnly}}
+            			fullWidth />
+				</Col>
+				<Col sm={5}>
+				 	<Autocomplete
+						options={currencies}
+						getOptionLabel={option => option.code}
+						renderOption={option => option.code}
+						renderInput={params => (
+						  <TextField
+							{...params}
+							label="Currency"
+							variant="outlined"
+							fullWidth
+							inputProps={{ ...params.inputProps }}
 						  />
-					</Col>
-				</Row>
-			</Container>
+						)}
+					  />
+				</Col>
+			</Row>
+		</Container>
 	);
 }

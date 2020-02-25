b6e6b9601078fad3b2f1586257b755580c0cb3fe
Timur Celik <timur.celik@gmx.de>
2020-02-25 18:52:01 +0100

    added logo to navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, {useState} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import HeaderBar from './components/HeaderBar';
 import CurrencyConverter from './components/CurrencyConverter';
--- src/components/HeaderBar.js
+++ src/components/HeaderBar.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import logo from '../logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+const appTitle = 'The Simple Currency Conversion';
 
 export default function HeaderBar() {
 
 	return (
 		<Navbar expand='lg' variant='dark' bg='dark'>
-		  <Navbar.Brand href='#home'>The Simple Currency Conversion</Navbar.Brand>
+		  <Navbar.Brand href='#home'>
+		  	<img
+				src={logo}
+				width='30'
+				height='30'
+				className="d-inline-block align-top"
+        		alt={appTitle} />
+				{appTitle}
+		  </Navbar.Brand>
 		  <Nav className='mr-auto'>
 			  <Nav.Link href='#home'>Home</Nav.Link>
+			  <Nav.Link href='#about'>About</Nav.Link>
 		  </Nav>
 		</Navbar>
 	);

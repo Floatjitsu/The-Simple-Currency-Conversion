e88d741300e26e2c70b1dc0cca37496de28b861a
Timur Celik <timur.celik@gmx.de>
2020-03-06 18:21:52 +0100

    added currency comparison module to route

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, BrowserRouter as Router } from 'react-router-dom';
 import CurrencyConverter from './components/CurrencyConverter';
 import HeaderBar from './components/HeaderBar';
 import AboutComponent from './components/AboutComponent';
+import CurrencyComparison from './components/CurrencyComparison';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <HeaderBar />
           <div className='App'>
             <Route exact path='/' component={CurrencyConverter} />
+            <Route exact path='/comparison' component={CurrencyComparison} />
             <Route path ='/about' component={AboutComponent} />
           </div>
       </Router>
---
+++ src/components/CurrencyComparison.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CurrencyComparison() {
+	return (
+		<div className='CurrencyComparison'>
+			This is the page for comparing different currencies :-)
+		</div>
+	)
+}
+
+export default CurrencyComparison;

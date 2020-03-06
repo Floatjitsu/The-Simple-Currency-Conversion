4c500bdb56d6b88f8b58132f7aed4256a91102c8
Timur Celik <timur.celik@gmx.de>
2020-03-06 18:56:47 +0100

    Added compare button to comparison container

--- src/components/CurrencyComparison.js
+++ src/components/CurrencyComparison.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import CurrencyInput from './CurrencyInput';
+import Button from '@material-ui/core/Button';
 
 function CurrencyComparison() {
 	return (
-		<div>
+		<div style={{display:'inline-flex', flexDirection:'column'}}>
 			<div className='CurrencyComparisonInfoTitle'>
 				Here you can compare up to 3 currencies against one base currency
 			</div>
@@ -16,26 +17,35 @@ function CurrencyComparison() {
 					currencyValueInputChangeCallBack={null}/>
 				<div className='ComparedCurrencies'>
 					<CurrencyInput
-						readOnly={false}
+						readOnly={true}
 						currencyValue={1}
 						defaultValue={'EUR'}
 						currencyNameInputChangeCallBack={null}
 						currencyValueInputChangeCallBack={null}/>
 					<div style={{marginTop:10}}/>
 					<CurrencyInput
-						readOnly={false}
+						readOnly={true}
 						currencyValue={1}
 						defaultValue={'EUR'}
 						currencyNameInputChangeCallBack={null}
 						currencyValueInputChangeCallBack={null}/>
 					<div style={{marginTop:10}}/>
 					<CurrencyInput
-						readOnly={false}
+						readOnly={true}
 						currencyValue={1}
 						defaultValue={'EUR'}
 						currencyNameInputChangeCallBack={null}
 						currencyValueInputChangeCallBack={null}/>
+					<div style={{marginTop:10}}/>
 				</div>
+			</div>
+			<div style={{alignSelf:'flex-end'}}>
+				<Button
+					variant='contained'
+					color='primary'
+					style={{marginRight: 15}}>
+					Compare
+				</Button>
 			</div>
 		</div>
 	);

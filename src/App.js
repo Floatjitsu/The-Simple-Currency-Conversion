847b02be1cf9a68814a4d9e7148404c968ff8a2d
Timur Celik <timur.celik@gmx.de>
2020-03-06 18:05:59 +0100

    moved Router to App Component and render App instead of Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
 import CurrencyConverter from './components/CurrencyConverter';
+import HeaderBar from './components/HeaderBar';
+import AboutComponent from './components/AboutComponent';
 
 function App() {
   return (
-      <div className='App'>
-        <CurrencyConverter/>
-      </div>
+      <Router>
+          <HeaderBar />
+          <div className='App'>
+            <Route exact path='/' component={CurrencyConverter} />
+            <Route path ='/about' component={AboutComponent} />
+          </div>
+      </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,23 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Route, BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import HeaderBar from './components/HeaderBar';
-import AboutComponent from './components/AboutComponent';
 
-const Routing = () => (
-	<Router>
-		<HeaderBar/>
-		<div>
-			<Route exact path='/' component={App} />
-			<Route path ='/about' component={AboutComponent} />
-		</div>
-	</Router>
-);
-
-ReactDOM.render(<Routing />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

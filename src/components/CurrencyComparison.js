66fd15aceee70db28b0a9e3def32d5233e85234e
Timur Celik <timur.celik@gmx.de>
2020-03-06 18:48:08 +0100

    built container for currency comparison

--- src/components/CurrencyComparison.js
+++ src/components/CurrencyComparison.js
@@ -1,11 +1,44 @@
 import React from 'react';
+import CurrencyInput from './CurrencyInput';
 
 function CurrencyComparison() {
 	return (
-		<div className='CurrencyComparison'>
-			This is the page for comparing different currencies :-)
+		<div>
+			<div className='CurrencyComparisonInfoTitle'>
+				Here you can compare up to 3 currencies against one base currency
+			</div>
+			<div className='CurrencyComparison'>
+				<CurrencyInput
+					readOnly={false}
+					currencyValue={1}
+					defaultValue={'EUR'}
+					currencyNameInputChangeCallBack={null}
+					currencyValueInputChangeCallBack={null}/>
+				<div className='ComparedCurrencies'>
+					<CurrencyInput
+						readOnly={false}
+						currencyValue={1}
+						defaultValue={'EUR'}
+						currencyNameInputChangeCallBack={null}
+						currencyValueInputChangeCallBack={null}/>
+					<div style={{marginTop:10}}/>
+					<CurrencyInput
+						readOnly={false}
+						currencyValue={1}
+						defaultValue={'EUR'}
+						currencyNameInputChangeCallBack={null}
+						currencyValueInputChangeCallBack={null}/>
+					<div style={{marginTop:10}}/>
+					<CurrencyInput
+						readOnly={false}
+						currencyValue={1}
+						defaultValue={'EUR'}
+						currencyNameInputChangeCallBack={null}
+						currencyValueInputChangeCallBack={null}/>
+				</div>
+			</div>
 		</div>
-	)
+	);
 }
 
 export default CurrencyComparison;
